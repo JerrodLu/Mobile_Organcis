70ea3b33da7672458bfaa23565bba94aa802eeb4
JerrodLu <103530882+JerrodLu@users.noreply.github.com>
2022-09-27 13:35:20 -0700

    have everything

---
+++ Organics2 - Copy/Assets/scripts/product_buttons.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class product_buttons : MonoBehaviour
+{
+    public float money = 0.0f;
+    public float netMoney = 0.0f;
+    public Text moneyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayMoney();
+    }
+
+    // buttons to earn money
+    public void FishQuater()
+    {
+        money += 5;
+        netMoney += 5;
+    }
+
+    public void FishGallon()
+    {
+        money += 20;
+        netMoney += 20;
+    }
+
+    public void FishTwoAndAHalf()
+    {
+        money += 50;
+        netMoney += 50;
+    }
+
+    public void FishBuckets()
+    {
+        money += 100;
+        netMoney += 100;
+    }
+
+    public void FishBarrel()
+    {
+        money += 1100;
+        netMoney += 1100;
+    }
+
+    public void DisplayMoney()
+    {
+        moneyText.text = money.ToString("C2");
+    }
+}
